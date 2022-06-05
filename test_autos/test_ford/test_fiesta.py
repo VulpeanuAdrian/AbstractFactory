ceea187587d0f1d26812b9a7b15f13b6e7c76888
Vulpeanu Adrian <vulpeanuadrian1994@gmail.com>
2022-06-05 19:19:04 +0100

    Fix unitest fail issue problem: due to wrong use of mock

--- test_autos/test_ford/test_fiesta.py
+++ test_autos/test_ford/test_fiesta.py
@@ -1,20 +1,17 @@
 import unittest
 from unittest.mock import patch, call
-
 from autos.ford.fiesta import FordFiesta
-
 
 class TestFiesta(unittest.TestCase):
     fiesta_test_obj = FordFiesta()
 
     @patch('builtins.print')
-    def test_start_fiesta(self):
-        assert TestFiesta.fiesta_test_obj.start() == [call('Ford Fiesta running cheaply.')]
+    def test_start_fiesta(self,mocked_print):
+        mocked_print.mock_calls == [call('Ford Fiesta running cheaply.'),TestFiesta.fiesta_test_obj.start()]
 
-    @staticmethod
     @patch('builtins.print')
-    def test_stop_fiesta(self):
-        assert TestFiesta.fiesta_test_obj.stop() == [call('Ford Fiesta shutting down.')]
+    def test_stop_fiesta(self,mocked_print):
+        mocked_print.mock_calls == [TestFiesta.fiesta_test_obj.stop(),call('Ford Fiesta shutting down.')]
 
 
 if __name__ == '__main__':
